a385beebea5ccb81eeb337b7f56abd11ef73ff22
Vitaliy Yudin <vitalij.iud@yandex.ru>
2024-04-08 14:07:37 +0300

    Добавлен close

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,8 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 import java.util.List;
 
 public class Main {
@@ -20,5 +22,6 @@ public class Main {
         }
         userService.cleanUsersTable();
         userService.dropUsersTable();
+        Util.closeConnection();
     }
 }
